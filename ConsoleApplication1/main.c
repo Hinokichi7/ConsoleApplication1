ec7d2594a7fa989bca28f5666881e10a4e3a3117
HarukaMori <faeriepunks7610@gmail.com>
2021-02-02 08:23:04 +0900

    2/2

--- ConsoleApplication1/main.c
+++ ConsoleApplication1/main.c
@@ -1,15 +1,7 @@
 #include <stdio.h>
 
 main() {
-	long			a;
-	unsigned int	b;
-	char			c;
+	char s[5] = { 'A', 'B', '\0', 'C' };
 
-	a = -120000;
-	b = 0x3FFF;
-	c = 015;
-
-	printf("value a is %ld \n", a);
-	printf("value b is %x \n", b);
-	printf("value c is %o \n", c);
+	printf("%s\n", s);
 }
