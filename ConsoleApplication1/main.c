c6e374eb278787ec28534133ac00f81279b18cb5
陽香 森 <faeriepunks7610@gmail.com>
2021-02-02 14:15:48 +0900

    2/2 2

--- ConsoleApplication1/main.c
+++ ConsoleApplication1/main.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
+#include <string.h>
 
 main() {
-	char s[5] = { 'A', 'B', '\0', 'C' };
+	char s[5] = { 'A', 'B', 'C', 'D', 0};
+	char s2[10];
+	strcpy_s(s2, 5, s);
 
-	printf("%s\n", s);
+	printf("%s\n", s2);
+
+	int a;
+	a = 16161;
+
+	printf("Number is %x in hexadecimal.\n", a);
+	printf("Number is %d in decimal.\n", a);
+	printf("Number is %o in octal.\n", a);
 }
