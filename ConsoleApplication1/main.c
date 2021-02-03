29ea756ef14992304802df093e1b796a33c149b1
HarukaMori <faeriepunks7610@gmail.com>
2021-02-04 08:35:12 +0900

    2/4

--- ConsoleApplication1/main.c
+++ ConsoleApplication1/main.c
@@ -2,12 +2,8 @@
 #include <string.h>
 
 main() {
-	static char a[25];
-	int b;
-
-	scanf_s("%s", a, 25);
-	scanf_s("%x", &b, 10);
-
-	printf(" String is : %s\n", a);
-	printf("Value is :%x\n", b);
+	int dx = 0;
+	printf("INPUT DATA :");
+	scanf_s("%d", &dx);
+	printf(">> %d\n", dx);
 }
