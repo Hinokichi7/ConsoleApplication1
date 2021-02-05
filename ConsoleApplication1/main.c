78fddb2a01ad8ddd7264538c65240d58c73d6291
陽香 森 <faeriepunks7610@gmail.com>
2021-02-05 12:56:32 +0900

    2/5

--- ConsoleApplication1/main.c
+++ ConsoleApplication1/main.c
@@ -2,16 +2,5 @@
 #include <string.h>
 
 main() {
-	int a, b;
 
-	scanf_s("%d", &a);
-	scanf_s("%d", &b);
-
-	if (a == b) {
-		printf("\n First == Second \n");
-	}
-	else
-	{
-		printf("\n First != Second \n");
-	}
 }
