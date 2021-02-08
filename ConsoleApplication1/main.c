afbf7a3541571c5554105d0a255ec70cce0dddbb
HarukaMori <faeriepunks7610@gmail.com>
2021-02-09 08:24:59 +0900

    2/9

--- ConsoleApplication1/main.c
+++ ConsoleApplication1/main.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 #include <string.h>
 
-static char number[21] = "12345678901234567890";
 main() {
-	int i;
-	i = 0;
-	while (i < 20)
-	{
-		printf("%c\n", number[i]);
-		i++;
-	}
+	int* a;
+	int b;
+
+	a = 100000000;
+	b = *a;
+	printf("%x", b);
 }
