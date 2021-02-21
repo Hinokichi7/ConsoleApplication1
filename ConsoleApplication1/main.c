7d5522cc0a39d5d3ad0e2447276a50a97a8c3577
HarukaMori <faeriepunks7610@gmail.com>
2021-02-22 08:29:07 +0900

    2/22

--- ConsoleApplication1/main.c
+++ ConsoleApplication1/main.c
@@ -1,12 +1,33 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
-int inputed(char comment[]) {
-	int val;
-	printf("%s", comment);
-	scanf_s("%d", &val);
-	return(val);
-}
 main() {
-	printf("\n Value = %d\n", inputed("Input value in decimal :"));
+	char file[64];
+	unsigned int i;
+	char baff[256];
+	FILE *fp;
+
+	printf("\n\n");
+	printf("Dump File-name : ");
+	scanf_s("%s", file, 60);
+	printf("\n");
+	printf("DUMP-FILE : %s\n\n", file);
+
+	fp = NULL;
+	if (0 != (fp = fopen_s(&fp, file, "r"))) {
+		printf(stderr, "\7\n Cannot Open File : %s\n\n", file);
+			exit(1);
+	}
+	else
+	{
+		for (i = 0; i < 65535; ++i) {
+			if (0 !=  fgets(baff, 255, fp)) {
+				break;
+			}
+			printf("%5d : %s", i+1, baff);
+		}
+	}
+
+	fclose(fp);
 }
