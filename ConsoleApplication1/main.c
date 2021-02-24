f6a76b5ed32beda5d70a3e9ab2d22e7638e79209
陽香 森 <faeriepunks7610@gmail.com>
2021-02-24 16:59:07 +0900

    2/24

--- ConsoleApplication1/main.c
+++ ConsoleApplication1/main.c
@@ -6,8 +6,9 @@ main() {
 	char file[64];
 	unsigned int i;
 	char baff[256];
-	FILE *fp = NULL;
+	FILE *fp;
 	errno_t error;
+	int ch;
 
 	printf("\n\n");
 	printf("Dump File-name : ");
@@ -15,20 +16,25 @@ main() {
 	printf("\n");
 	printf("DUMP-FILE : %s\n\n", file);
 
-	
-	if ((error = fopen_s(&fp, file, "r")) != 0) {
-		printf(stderr, "\7\n Cannot Open File : %s\n\n", file);
-			exit(1);
-	}
-	else
-	{
-		fp = error;
+
+	error = fopen_s(&fp, file, "r");
+
+	if (error == 0) {
+		//while ((ch = fgetc(fp)) != EOF) {
+		//	putchar(ch);
+		//}
 		for (i = 0; i < 65535; ++i) {
-			if ((fgets(baff, 255, fp)) != 0) {
+			if ((fgets(baff, 255, fp)) != NULL) {
+				printf("%5d : %s", i + 1, baff);
+			}
+			else
+			{
 				break;
 			}
-			printf("%5d : %s", i+1, baff);
 		}
+	}
+	else {
+		printf("Cannot Open File : %s\n", file);
 	}
 
 	fclose(fp);
